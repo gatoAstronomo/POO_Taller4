645d903560e653db4d377462e5a314cb1fa08599
HectorBecerra <h.becerra01@ufromail.cl>
2024-06-27 11:30:59 -0400

    funcion Saveplayer

--- src/main/java/Data/DataPlayer.java
+++ src/main/java/Data/DataPlayer.java
@@ -1,9 +1,13 @@
 package Data;
 
 import Model.Player;
+import Model.Position;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -12,7 +16,35 @@ public class DataPlayer {
         ArrayList<Player> players = new ArrayList<>();
         File doc = new File(pathTeam);
         Scanner obj = new Scanner(doc);
-        while (obj.hasNextLine()) System.out.println(obj.nextLine());
+
+        while (obj.hasNextLine()) {
+            String line = obj.nextLine();
+            String[] data = line.split(";");
+            if (data.length == 3) {
+                Player player = new Player();
+                player.setNumber(data[0].trim());
+                player.setName(data[1].trim());
+                try {
+                    player.setPosition(Position.valueOf(data[2].trim()));
+                } catch (IllegalArgumentException e) {
+                    e.printStackTrace();
+                }
+                players.add(player);
+            }
+        }
+        obj.close();
         return players;
+    }
+
+    public void savePlayers(String pathTeam, ArrayList<Player> players) throws IOException {
+        FileWriter fileWriter = new FileWriter(pathTeam);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+
+        for (Player player : players) {
+            String line = String.format("%s;%s;%s", player.getNumber(), player.getName(), player.getPosition().name());
+            printWriter.println(line);
+        }
+
+        printWriter.close();
     }
 }
