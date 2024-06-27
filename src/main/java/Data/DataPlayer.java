f61cd05530ea8b4a96f3cac50ecfcb220a1547b9
Cris <c.cheuquel01@ufromail.cl>
2024-06-27 12:05:07 -0400

    DataPlayer advanced

--- src/main/java/Data/DataPlayer.java
+++ src/main/java/Data/DataPlayer.java
@@ -2,31 +2,17 @@ package Data;
 
 import Model.Player;
 import Model.Position;
+import Model.Team;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DataPlayer {
-    public static ArrayList<Player> loadPlayers(String pathTeam) throws FileNotFoundException {
-        ArrayList<Player> players = new ArrayList<>();
-
-        try {
-            File doc = new File(pathTeam);
-            Scanner obj = new Scanner(doc);
-            while (obj.hasNextLine()) {
-                String line = obj.nextLine();
-                Player player = extractPlayer(line);
-                players.add(player);
-            }
-        }catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-
-        return players;
-    }
 
     public static Player extractPlayer(String linea) {
         String[] data = linea.split(";");
@@ -35,5 +21,38 @@ public class DataPlayer {
         Position position = Position.valueOf(data[2]);
         return new Player(number, name, position);
     }
+    public static List<Team> loadTeams(String filePath) {
+        List<Team> teams = new ArrayList<>();
+        try (Stream<String> lines = Files.lines(Paths.get(filePath))) {
+            teams = lines.map(line -> {
+                String[] data = line.split(";");
+                String name = data[1];
+                String ranking = data[2];
+                String imagePath = data[3];
+                List<Player> players = new ArrayList<>();
+                return new Team(name, ranking, imagePath, (ArrayList<Player>) players);
+            }).collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        return teams;
+    }
+    public static ArrayList<Player> loadPlayers(String pathTeam) throws FileNotFoundException {
+        ArrayList<Player> players = new ArrayList<>();
+
+        try (Stream<String> lines = Files.lines(Paths.get(pathTeam))) {
+            players = lines.map(DataPlayer::extractPlayer)
+                    .collect(Collectors.toCollection(ArrayList::new));
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        return players;
+    }
+    public static ArrayList<Team> getTeams(){
+        String pathTeams = "src/main/java/Data/datos/teams.txt";
+        return (ArrayList<Team>) loadTeams(pathTeams);
+    }
 }
--- src/main/java/Main/Launcher.java
+++ src/main/java/Main/Launcher.java
@@ -1,7 +1,15 @@
-package proyect;
+package Main;
+
+import Data.DataPlayer;
+import Model.Player;
+import Model.Team;
+
+import java.util.ArrayList;
 
 public class Launcher {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        // System.out.println("Hi");
+        ArrayList<Team> data = DataPlayer.getTeams();
+        data.stream().forEach(team ->System.out.println(team.getName()));
     }
 }
